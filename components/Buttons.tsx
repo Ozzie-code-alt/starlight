60dff1038120174da766063b811951a3c609d68b
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-12-30 14:46:16 +0800

    Add: Navbar

---
+++ components/Buttons.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Image from 'next/image'
+type ButtonProps = {
+type: 'button' | 'submit'
+title: string
+icon?: string
+variant: 'btn_dark_green'
+}
+
+
+const Buttons = ({type,
+title,
+icon,
+variant}:ButtonProps) => {
+  return (
+   <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`}> {/*Dynamic template strings*/}
+      {icon && <Image src={icon} alt={title} width={24} height={24} /> } {/*if icon exists then we render image */}
+      <label className='bold-16 whitespace-nowrap'>{title}</label>
+   </button>
+  )
+}
+
+export default Buttons
