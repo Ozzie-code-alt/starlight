479a214755d7f00523bd6fc398a91b5074c3f6bc
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-01-04 23:43:56 +0800

    Worked ON:Section 2

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+const Campsite = ()=>{
+  return(
+    <div>
+      <h1>Campsite 1 </h1>
+    </div>
+  )
+}
+
+
 
 const Camp = () => {
   return (
-    <div>Camp</div>
+    <section className=' border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:m-20'> 
+          <div className='hide-scrollbbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
+      <Campsite
+      backgroundImage = ""
+      />
+      <Campsite/>
+          </div>
+    </section>
   )
 }
 
