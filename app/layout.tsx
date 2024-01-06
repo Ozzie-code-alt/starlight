529f33d0a3b776df3c28f1c671f387cd87e66d3b
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-01-06 13:36:40 +0800

    Finished: Section 3 Add: Splash Screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Providers from "./providers";
 
+
 export const metadata: Metadata = {
   title: "Travel",
   description: "Travel UI/UX App for Camping",
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+"use client"
 import Camp from "@/components/Camp";
 import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
+import TransitionEffect from "@/components/TransitionEffect";
+
 
 export default function Home() {
   return (
     <>
+    <TransitionEffect/>
     <Hero/>
     <Camp/>
     <Guide/>
