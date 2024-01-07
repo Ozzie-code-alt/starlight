3283d3aea9cea0068b052f1a330db656842380b8
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-01-07 10:13:02 +0800

    Finished:Section 5 CTA

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -6,17 +6,19 @@ type: 'button' | 'submit'
 title: string
 icon?: string
 variant: string
+full?: boolean
 }
 
 
 const Buttons = ({type,
 title,
 icon,
+full,
 variant}:ButtonProps) => {
   return (
-   <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`}> {/*Dynamic template strings*/}
+   <button type={type} className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}> {/*Dynamic template strings*/}
       {icon && <Image src={icon} alt={title} width={24} height={24} /> } {/*if icon exists then we render image */}
-      <label className='bold-16 whitespace-nowrap'>{title}</label>
+      <label className='bold-16 whitespace-nowrap cursor-pointer'>{title}</label>
    </button>
   )
 }
--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import React from "react";
+import Buttons from "./Buttons";
+import Image from "next/image";
 const GetApp = () => {
   return (
-    <div>GetApp</div>
-  )
-}
+    <section className="flexCenter w-full flex-col pb-[100px]">
+      <div className="get-app">
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for Free Now</h2>
+          <p className="regular-16 text-gray-10">Available on IOS and Android</p>
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
+            <Buttons type="button"
+            title="Download App"
+            icon="/apple.svg"
+            variant="btn_white"
+            full
+            />
+          
+            <Buttons type="button"
+            title="Download App"
+            icon="/android.svg"
+            variant="btn_dark_green_outline"
+            full
+            />
+          
+          </div>
+        </div>
 
-export default GetApp
+
+        <div className="flex flex-1 items-center justify-end">
+            <Image
+            src={"/phones.png"}
+            alt="phone"
+            width={550}
+            height={870}
+            />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetApp;
